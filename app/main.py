21aff6f608a9379b8a16cdf8f8b31ecbb4044c8e
Szeri <szeri323@gmail.com>
2024-06-04 19:10:01 +0200

    pass stage

--- app/main.py
+++ app/main.py
@@ -9,9 +9,16 @@ def main():
     # Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-    #print(server_socket)
-    server_socket.accept()[0].sendall(b'HTTP/1.1 200 OK\r\n\r\n')
+    
+    while True:
+        client_socket, client_adress = server_socket.accept()
+        print(f"New connection from {client_adress}")
+        
+        response = b'HTTP/1.1 200 OK\r\n\r\n'
+        
+        client_socket.sendall(response)
+        
+        client_socket.close()
 
 if __name__ == "__main__":
     main()
