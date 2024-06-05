e07785df96c72dcfaaeaca3d6f32a0a201749e68
Szeri <szeri323@gmail.com>
2024-06-05 11:43:49 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,19 +9,23 @@ def main():
     # Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    
+
     while True:
         client_socket, client_adress = server_socket.accept()
         print(f"New connection from {client_adress}")
         print(f"Socket: {client_socket}")
         
-        recived = client_socket.recv(1024)
+        client_header = client_socket.recv(1024)
+        client_header_args = client_header.decode()
+        client_header_args =  client_header_args.split(" ")
+        method, path, protocol, host = tuple(client_header_args)
+        path_strings = path.split('/')
         
-        if recived == b'GET / HTTP/1.1\r\nHost: localhost:4221\r\n\r\n':
-            print(f"recived: {recived}")
+        if path == '/':
             response = b'HTTP/1.1 200 OK\r\n\r\n'
+        elif "echo" in path:
+            response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path_strings[2])} \r\n\r\n{path_strings[2]}'.encode()
         else:
-            print(f"recived: {recived}")
             response = b'HTTP/1.1 404 Not Found\r\n\r\n'
         
         
